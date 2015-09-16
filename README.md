8404afb6e6c6e218f519440aed575291ec6cbb83
Malcolm Studd <mstudd@recognia.com>
2015-09-16 14:01:39 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+MetaCPAN::Walker - Walk release dependencies using MetaCPAN
+
+# SYNOPSIS
+
+    use MetaCPAN::Walker;
+
+# DESCRIPTION
+
+MetaCPAN::Walker provides easy ways to walk sets of CPAN releases.
+
+# AUTHOR
+
+Malcolm Studd &lt;mestudd@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2015- Recognia Inc.
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/MetaCPAN/Walker.pm
@@ -0,0 +1,39 @@
+package MetaCPAN::Walker;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+MetaCPAN::Walker - Walk release dependencies using MetaCPAN
+
+=head1 SYNOPSIS
+
+  use MetaCPAN::Walker;
+
+=head1 DESCRIPTION
+
+MetaCPAN::Walker provides easy ways to walk sets of CPAN releases.
+
+=head1 AUTHOR
+
+Malcolm Studd E<lt>mestudd@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015- Recognia Inc.
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
